9d796460389fee562c7497b6498621ca57171a4e
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-11 01:50:17 -0500

    Driver outline started

--- project3_guess_who.py
+++ project3_guess_who.py
@@ -6,7 +6,18 @@ def main():
         Driver Method
     """
     kb = KnowledgeBase()
-    
+
+    print("======================================")
+    print(f"Looking for {kb.the_character['Name']}.")
+    print(f"{kb.the_character['Name']} looks like this: {kb.the_character}")
+    print("The population has the possible set of characteristics: ", kb.get_variable_characteristics())
+    print("======================================")
+    print("Starting interview... ")
+    qCount = 0
+    while(True):
+        print("======================================")
+        qCount += 1
+        print(f"Question # {qCount}")
 
 
 main()
