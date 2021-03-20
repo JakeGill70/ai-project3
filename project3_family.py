180476e217ebca5a56e7eddb5226c702e76cb08c
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-20 14:20:38 -0400

    Interactive driver

--- project3_family.py
+++ project3_family.py
@@ -27,9 +27,107 @@ def makeRelationshipOutput(relationships, fileName=None):
         sys.stdout = normalOutput
 
 
+def processInput(rawInput):
+    # Define output values
+    relationship = None
+    personX = None
+    personY = None
+
+    # Request format: "RELATIONSHIP PERSON_X PERSON_Y"
+    request = rawInput.split(" ")
+    if(len(request) == 3):
+        relationship = request[0]
+        personX = request[1]
+        personY = request[2]
+
+    # Return the results as a dictionary because tuples cause troubles
+    return {"relationship": relationship, "x": personX, "y": personY}
+
+
+def verifyInput(processedInput, relationships):
+    # Get the values from processedInput()'s dicionary
+    relationship = processedInput["relationship"]
+    personX = processedInput["x"]
+    personY = processedInput["y"]
+
+    # Assume the input is valid until proven otherwise
+    isValidInput = True
+
+    # Is the relationship defined?
+    if(relationship not in relationships.dispatchFunction.keys()):
+        print(
+            f"Relationship not defined. Relationship must be: {list(relationships.dispatchFunction.keys())}",
+            file=sys.stderr)
+        isValidInput = False
+
+    # Does person_x exist?
+    if(not relationships.findCharacter(personX)):
+        print(
+            f"{personX} is not defined. Person X must be: {[c['Name'] for c in relationships.getCharacters()]}",
+            file=sys.stderr)
+        isValidInput = False
+
+    # Does person_y exist?
+    if(not relationships.findCharacter(personY)):
+        print(
+            f"{personY} is not defined. Person X must be: {[c['Name'] for c in relationships.getCharacters()]}",
+            file=sys.stderr)
+        isValidInput = False
+
+    return isValidInput
+
+
+def processRequest(rawInput, relationships):
+
+    # Define output values
+    relationship = None
+    personX = None
+    personY = None
+    result = None
+
+    # Process the raw input into a easy to use dictionary
+    processedInput = processInput(rawInput)
+
+    # Update the output values
+    relationship = processedInput["relationship"]
+    personX = processedInput["x"]
+    personY = processedInput["y"]
+
+    # Verify that the request has usable values
+    verifiedInput = verifyInput(processedInput, relationships)
+
+    # Get the result if the request had possible values
+    if(verifiedInput):
+        result = relationships.dispatchFunction[relationship](personX, personY)
+
+    # Return the results as a dictionary because tuples cause troubles
+    return {"relationship": relationship, "x": personX, "y": personY, "result": result}
+
+
 def main():
     relationships = Relationships()
     makeRelationshipOutput(relationships, "RelationshipOutput.csv")
+
+    print("\nRequest Format: 'RELATIONSHIP PERSON_X PERSON_Y'")
+
+    while(True):
+        # Get request as raw input
+        print("\nWhat relationship would you like to know about?")
+        rawInput = input()
+
+        # Safe exit for the program
+        if(rawInput == "exit"):
+            break
+
+        # Process the request
+        request = processRequest(rawInput, relationships)
+
+        # If the request lead to a result, print it, otherwise, inform that it wasn't a good request
+        if(request["result"] != None):
+            print(
+                f"({request['result']}) {request['y']} is {'NOT ' if not request['result'] else ''}{request['x']}'s {request['relationship']}.")
+        else:
+            print("That request could not be processed.")
 
 
 main()
