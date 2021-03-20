274e84bfd937ac047ff5e8e2755e675e485d75fa
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-20 13:29:13 -0400

    Write relationships to csv

--- project3_family.py
+++ project3_family.py
@@ -1,9 +1,35 @@
 from Relationships import Relationships
+import sys
+
+
+def makeRelationshipOutput(relationships, fileName=None):
+
+    FILE_OUTPUT = open(fileName, "w") if fileName else None
+
+    if(FILE_OUTPUT):
+        normalOutput = sys.stdout
+        sys.stdout = FILE_OUTPUT
+
+    print("Relationship_Name,Person_x,Person_Y")
+
+    for r in relationships.dispatchFunction.keys():
+        for personX in relationships.getCharacters():
+            x = personX["Name"]
+            for personY in relationships.getCharacters():
+                y = personY["Name"]
+
+                related = relationships.dispatchFunction[r](x, y)
+                if(related):
+                    print(r, x, y)
+
+    if(FILE_OUTPUT):
+        FILE_OUTPUT.close()
+        sys.stdout = normalOutput
 
 
 def main():
-    r = Relationships()
-    print(r.isParent("Anita", "Paul"))
+    relationships = Relationships()
+    makeRelationshipOutput(relationships, "RelationshipOutput.csv")
 
 
 main()
