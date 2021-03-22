77acd6a5d02df7bce0a65afb434b807b0559a918
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-22 15:33:43 -0400

    More in-line documentation

--- Relationships.py
+++ Relationships.py
@@ -36,6 +36,9 @@ class Relationships:
         try:
             return [char for char in self.getCharacters() if char["Name"] == name][0]
         except IndexError:
+            # An index error occurs if the first list is empty, no element [0].
+            # If the list is empty, then no character has that name, and we may
+            # just return None.
             return None
 
     def isMale(self, x):
@@ -58,6 +61,7 @@ class Relationships:
 
 # Sibling
     def isSibling(self, x, y):
+        # Siblings is true if x and y share any parent
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isParent(x, z) and self.isParent(y, z)):
@@ -66,6 +70,7 @@ class Relationships:
 
 # Spouse
     def isSpouse(self, x, y):
+        # Spouce is true if x and y share any child
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isParent(z, x) and self.isParent(z, y)):
@@ -74,42 +79,49 @@ class Relationships:
 
 # Niece
     def isNiece(self, x, y):
+        # Get y's parents
         yParents = []
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isParent(y, z)):
                 yParents.append(z)
 
+        # Determine if y's parents are siblings of x
         yParentIsSibling = False
         for z in yParents:
             if(self.isSibling(x, z)):
                 yParentIsSibling = True
                 break
 
+        # Return true if y's parents are siblings of x, and x is female
         isNiece = yParentIsSibling and self.isFemale(y)
 
         return isNiece
 
 # Nephew
     def isNephew(self, x, y):
+        # Get y's parents
         yParents = []
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isParent(y, z)):
                 yParents.append(z)
 
+        # Determine if y's parents are siblings of x
         yParentIsSibling = False
         for z in yParents:
             if(self.isSibling(x, z)):
                 yParentIsSibling = True
                 break
 
+        # Return true if y's parents are siblings of x, and x is male
         isNephew = yParentIsSibling and self.isMale(y)
 
         return isNephew
 
 # Cousin
     def isCousin(self, x, y):
+        # Get both x's and y's parent
         xParents = []
         yParents = []
         for char in self.getCharacters():
@@ -119,12 +131,14 @@ class Relationships:
             if(self.isParent(y, z)):
                 yParents.append(z)
 
+        # Determine if any of x's parents are siblings to any of y's parents
         parentsAreSiblings = False
         for xp in xParents:
             for yp in yParents:
                 if(self.isSibling(xp, yp)):
                     parentsAreSiblings = True
 
+        # x and y are 1st cousins if either if any parent between x and y are siblings
         return parentsAreSiblings
 
 # Sister
@@ -137,16 +151,19 @@ class Relationships:
 
 # Grandparent
     def isGrandparent(self, x, y):
+        # Get x's parents
         xParents = []
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isParent(x, z)):
                 xParents.append(z)
 
+        # Return true if either of x's parents has y for a parent
         for xParent in xParents:
             if(self.isParent(xParent, y)):
                 return True
 
+        # Return false if none of x's parents has y for a parent
         return False
 
 # Grandfather
@@ -159,16 +176,19 @@ class Relationships:
 
 # Great-Grandparent
     def isGreatGrandparent(self, x, y):
+        # Get x's grandparents
         xGrandParents = []
         for char in self.getCharacters():
             z = char["Name"]
             if(self.isGrandparent(x, z)):
                 xGrandParents.append(z)
 
+        # Return true if any of x's grandparents has y as a parent
         for xGrandParent in xGrandParents:
             if(self.isParent(xGrandParent, y)):
                 return True
 
+        # Return false if none of x's grandparents has y as a parent
         return False
 
 # Great-Grandmother
